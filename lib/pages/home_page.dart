a682f65cff7478631f2821a7fc0341305009d44f
tong <twang423@gatech.edu>
2019-02-28 20:12:22 -0500

    Drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:teamup_app/services/authentication.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomePage extends StatefulWidget{
-  HomePage({Key key, this.auth, this.userId, this.onSignedOut}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.onSignedOut})
+      : super(key: key);
 
   final BaseAuth auth;
   final VoidCallback onSignedOut;
@@ -11,19 +12,15 @@ class HomePage extends StatefulWidget{
 
   @override
   State<StatefulWidget> createState() => new _HomePageState();
-
 }
 
-class _HomePageState extends State<HomePage>{
-
+class _HomePageState extends State<HomePage> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final db = Firestore.instance;
-
 
   @override
   void initState() {
     super.initState();
-
   }
 
   _signOut() async {
@@ -35,27 +32,57 @@ class _HomePageState extends State<HomePage>{
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Home Page'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            new RaisedButton(
+        appBar: new AppBar(
+          title: new Text('Home Page'),
+        ),
+        body: Center(
+            child: Column(children: [
+          new RaisedButton(
               onPressed: _signOut,
               color: Colors.blue,
               elevation: 5,
-              child: new Text('Sign Out', style: new TextStyle(color: Colors.white),)
-              )
-            ]
-          )
-        )
-      );
+              child: new Text(
+                'Sign Out',
+                style: new TextStyle(color: Colors.white),
+              ))
+        ])),
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the Drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ));
   }
-
-
 }
