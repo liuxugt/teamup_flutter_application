55b957e5edc0e3262d8119d2b0e1af886ee3609f
Xu Liu <liuxugt@gmail.com>
2019-03-20 22:25:38 -0400

    add basic implementation of notification

--- lib/objects/course.dart
+++ lib/objects/course.dart
@@ -6,6 +6,8 @@ class Course {
   String _id;
   CollectionReference _membersRef;
   CollectionReference _teamsRef;
+  CollectionReference _applicationRef;
+  CollectionReference _invitationRef;
 
 
 
@@ -15,7 +17,8 @@ class Course {
     _id = courseSnap.documentID;
     _membersRef = courseSnap.reference.collection('members').reference();
     _teamsRef = courseSnap.reference.collection('teams').reference();
-
+    _applicationRef = courseSnap.reference.collection('applications').reference();
+    _invitationRef = courseSnap.reference.collection('invitations').reference();
   }
 
 
@@ -25,15 +28,17 @@ class Course {
   CollectionReference get membersRef => _membersRef;
 //  CollectionReference get projectsRef => _projectsRef;
   CollectionReference get teamsRef => _teamsRef;
+  CollectionReference get applicationRef => _applicationRef;
+  CollectionReference get invitationRef => _invitationRef;
 
 
   //TODO: potentially do the conversion here from QuerySnapshot to Team here and create a stream<Team> that is fed to the pages for better abstraction
   Stream<QuerySnapshot> get availableTeamsStream => _teamsRef.where("available_spots", isGreaterThan: 0).snapshots();
   Stream<QuerySnapshot> get unavailableTeamsStream => _teamsRef.where("available_spots", isEqualTo: 0).snapshots();
   Stream<QuerySnapshot> get availableMembersStream => _membersRef.orderBy("team").snapshots();
-
-
-
-
+  Stream<QuerySnapshot> get pendingApplicationStream => _applicationRef.where("status", isEqualTo: "pending").snapshots();
+  Stream<QuerySnapshot> get respondedApplicationStream => _applicationRef.where("status", isEqualTo: "responded").snapshots();
+  Stream<QuerySnapshot> get pendingInvitationStream => _invitationRef.where("status", isEqualTo: "pending").snapshots();
+  Stream<QuerySnapshot> get respondedInvitationStream => _invitationRef.where("status", isEqualTo: "responded").snapshots();
 
 }
